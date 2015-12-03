11e10275c9e9fc984bef65dfbc19abf6c32c3779
Nathan Tippy <nathan@tippy.name>
2015-12-03 12:35:53 -0600

    Initial hello world check in.

---
+++ src/main/java/com/ociweb/protocoltest/App.java
@@ -0,0 +1,23 @@
+package com.ociweb.protocoltest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    //Put this line at the top of every class and be sure to change the Class name to that of the class in question.
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    
+    public static void main(String[] args) {
+       
+        log.info("Hello World, we are running...");
+        
+        
+        
+    }
+    
+    
+    
+    
+
+}
